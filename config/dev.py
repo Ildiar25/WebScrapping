97741343f28e62326489d68345b1fd4495963652
Joan <j.pastor1591@gmail.com>
2025-04-18 10:30:52 +0200

    Added config files

---
+++ config/dev.py
@@ -0,0 +1,6 @@
+from typing import Final
+
+DEBUG: Final[bool] = True
+SECRET_KEY: Final[str] = "Secret_KEY"
+SQLALCHEMY_DATABASE_URI: Final[str] = "sqlite:///../instance/database.db"
+SQLALCHEMY_TRACK_MODIFICATIONS: Final[bool] = False
